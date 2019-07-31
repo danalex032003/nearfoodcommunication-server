9515b66171b4e8961fe08538a61597478262d2ca
Alexandru <danalex032003@gmail.com>
2019-07-31 10:40:32 +0300

    initial commit
    :wq

---
+++ src/main/java/com/nearfoodcommunication/menu/model/FoodItem.java
@@ -0,0 +1,50 @@
+package com.nearfoodcommunication.menu.model;
+
+public class FoodItem {
+	private int id;
+	private String name;
+	private double price;
+	private String description;
+	private int weight;// in gramms
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+}
