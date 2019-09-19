065d9a2979b2fa93b4e68bb28273e2d3a0dc5e7e
Flaviu Fildan <flaviu.fildan@arobs.com>
2019-09-19 17:00:52 +0300

    Order mock controller

---
+++ src/main/java/com/nearfoodcommunication/order/controller/OrderController.java
@@ -0,0 +1,19 @@
+package com.nearfoodcommunication.order.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+	@PostMapping(value = "/order")
+	public OrderResponse saveOrder(@RequestBody String order) {
+
+		OrderResponse orderResponse = new OrderResponse();
+		orderResponse.setOrderBody( order );
+
+		return orderResponse;
+	}
+
+}
