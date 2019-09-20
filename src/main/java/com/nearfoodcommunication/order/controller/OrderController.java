e3fec7f18c2ebf10815b41dfa82cc99f9552b7b5
Flaviu Fildan <flaviu.fildan@arobs.com>
2019-09-20 18:05:04 +0300

    revert mock Order controller

--- src/main/java/com/nearfoodcommunication/order/controller/OrderController.java
+++ src/main/java/com/nearfoodcommunication/order/controller/OrderController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class OrderController {
 
-	@PostMapping(value = "/order")
+//	@PostMapping(value = "/order")
 	public OrderResponse saveOrder(@RequestBody String order) {
 
 		OrderResponse orderResponse = new OrderResponse();
